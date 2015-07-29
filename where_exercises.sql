42224cc5b6f4b6b62e781df6845d51ffb11a8b86
Dylan Perenchio <perenchiod@gmail.com>
2015-07-29 14:26:27 -0500

    Updated my exercise to different format

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,22 +1,28 @@
+SELECT concat (first_name, ' ', last_name)
+FROM employees
+WHERE first_name = 'Irena'
+OR first_name = 'Vidya'
+OR first_name = 'Maya';
+
+SELECT first_name , last_name , gender
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+AND gender = 'M';
+
+SELECT concat(first_name, ' ', last_name)
+FROM employees
+WHERE last_name LIKE 'E%'
+OR last_name LIKE '%E';
+
 SELECT *
 FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya');
-
-SELECT concat(first_name, ' ', last_name) AS full_name
-FROM employees
-WHERE last_name like 'E%';
+WHERE hire_date LIKE '199%'
+AND birth_date LIKE '%-12-25';
 
 SELECT *
 FROM employees
-WHERE birth_date like '%-12-25';
-
-SELECT *
-FROM employees
-WHERE last_name LIKE '%q%';
-
-SELECT *
-FROM employees
-WHERE hire_date LIKE '199%';
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%';
 
 
 
